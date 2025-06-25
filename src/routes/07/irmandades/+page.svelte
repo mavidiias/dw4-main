47c4f672759db4483fbe428b9b994abb20263fb0
mavidiias <mavi.dias2008@gmail.com>
2025-06-25 12:32:47 -0400

    tem que terminar o cap 6 e agr 7

---
+++ src/routes/07/irmandades/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    let {form} = $props();
+</script>
+
+<form method="post">
+    Nome: <input name="nome" type="text" placeholder="Nome do Apendiz" /><br />
+    Nascimento: <input name="nascimento" type="date" /><br />
+    Força: <input name="forca" type="number" min="1" max="10" /><br />
+    Inteligência: <input name="inteligencia" type="number" min="1" max="10" /><br />
+    Destreza: <input name="destreza" type="number" min="1" max="10" /><br />
+    Conhecimento de magia: <input name="magia" type="checkbox" /><br />
+    Ferramentas de Artesão: <input name="ferramentas" type="checkbox" /><br />
+    <button formaction="?/guerreiros">Inscrever na Ordem dos Gerreiros</button> <br>
+    <button formaction="?/magos">Inscrever no Círculo dos Magos</button> <br>
+    <button formaction="?/artesaos">Inscrever na na Guilda dos Artesãos</button> <br>
+</form>
+
+{#if form?.error}
+   <p style="color: red">{form.error}</p>
+{/if}
